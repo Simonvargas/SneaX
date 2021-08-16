7ed505927256dd380dc5a13dfbc2975a1e36f548
june <luo.jnny@gmail.com>
2021-08-16 10:19:55 -0700

    added get route for api/watch_route.py

--- app/api/watch_route.py
+++ app/api/watch_route.py
@@ -1,3 +1,4 @@
+from app.api.sneax_routes import sneax
 from flask import Blueprint
 from flask_login import login_required, current_user
 from app.models import Watch
@@ -7,7 +8,9 @@ watch_routes = Blueprint("watchs", __name__)
 @watch_routes.route("/") 
 @login_required
 def watchs():
+    watchs = Watch.query.all()
+    return{'watchs': [watch.to_dict() for watch in watchs]}
     
-# get route - watch query.all -dic 
 
+@
 # post route 
