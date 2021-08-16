7a6f646846efc644dde90a658eb3e2e10c6b93db
Vivian C <baongocche@gmail.com>
2021-08-16 15:35:23 -0700

    navbar and splashpage

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import { useSelector } from 'react-redux';
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
@@ -10,10 +10,21 @@ const NavBar = () => {
     <div>
     {!sessionUser ?
     <nav className={styles.navBar}>
-      <div>hello</div>
+      <div className={styles.logo}>
+        <img className={styles.photo} src='https://i.imgur.com/mu6iY5g.png'></img>
+      </div>
+      <div className={styles.btnContainer}>
+      <NavLink to='/login'>
+      <button className={`${styles.btn} ${styles.btn1}`}>Log in</button>
+      </NavLink>
+      <NavLink to='/signup'>
+      <button className={`${styles.btn} ${styles.btn2}`}>Sign up</button>
+      </NavLink>
+      </div>
     </nav> :
     <nav>
        <li>
+
           <NavLink to='/' exact={true}>
           <LogoutButton />
           </NavLink>
@@ -25,28 +36,3 @@ const NavBar = () => {
 }
 
 export default NavBar;
-
-
-{/* <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-      </ul> */}
