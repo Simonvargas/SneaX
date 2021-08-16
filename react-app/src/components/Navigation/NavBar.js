ac79ee996184e065cf649dd03466c8351fb63dc6
simonvargas <simonvargas01@gmail.com>
2021-08-16 18:31:09 -0400

    navbar

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -11,10 +11,21 @@ const NavBar = () => {
     <div>
     {!sessionUser ?
     <nav className={styles.navBar}>
-      <div>hello</div>
+      <div className={styles.logo}>
+        <img className={styles.photo} src='https://i.imgur.com/mu6iY5g.png'></img>
+      </div>
+      <div className={styles.btnContainer}>
+      <NavLink to='/login'>
+      <button className={`${styles.btn} ${styles.btn1}`}>Log in</button>
+      </NavLink>
+      <NavLink to='/signup'>
+      <button className={`${styles.btn} ${styles.btn2}`}>Sign up</button>
+      </NavLink>
+      </div>
     </nav> :
     <nav>
        <li>
+
           <NavLink to='/' exact={true}>
           <LogoutButton />
           </NavLink>
