d925452d2b44af5b62c84485bca9d376a8a30e6b
june <luo.jnny@gmail.com>
2021-08-16 11:04:29 -0700

    worked on the WatchForm

--- app/forms/watch_form.py
+++ app/forms/watch_form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import IntegerField, SubmitField
 
 
 class WatchForm(FlaskForm):
-    watchlist_id = IntegerField('watchlist_id', [validators.DataRequired(]))
-    sneax_id = IntegerField('sneax_id', [validators.DataRequired(]))
+    watchlist_id = IntegerField('watchlist_id', [validators.DataRequired()])
+    sneax_id = IntegerField('sneax_id', [validators.DataRequired()])
+    submit = SubmitField("Add to Watchlist")
+
