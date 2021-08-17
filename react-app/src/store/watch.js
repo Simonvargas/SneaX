47b5c9fd0a6772edde1c39280e49d53abadcae09
june <luo.jnny@gmail.com>
2021-08-17 09:37:33 -0700

    watch thunk creator inprogress

--- react-app/src/store/watch.js
+++ react-app/src/store/watch.js
@@ -24,19 +24,23 @@ const removeWatch = (watch) => {
         watch
     };
 }
-// action thunk
+
+
+
+// Thunks 
 export const addOneWatch = (payload) => async(dispatch) => {
     const res = await fetch('/api/watch/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(payload),
     })
-    if(res.ok) {
-        const watch = await res.json();
-        dispatch(addWatch(watch));
-        return watch;
-    }
+    if(!res.ok) throw res
+    const watch = await res.json();
+    dispatch(addWatch(watch));
+    return watch;
+    
 }
+
 
 export const getWatchs = () => async(dispatch) => {
     const res = await fetch('/api/watch/');
@@ -44,16 +48,53 @@ export const getWatchs = () => async(dispatch) => {
     dispatch(loadWatch(watchs));
 }
 
+export const getOneWatch = (id) => async(dispatch) => {
+    const res = await fetch(`/api/watch/${id}`);
+    if(res.ok){
+        const watch = await res.json();
+        dispatch(loadWatch(watch));
+         return watch;
+    }
+}
 
 
+export const removeOneWatch = (id) => async(dispatch) => {
+    const res = await fetch(`/api/watch/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({id}),
+    });
+    if(!res.ok) throw res;
+    const watch = await res.json();
+    dispatch(removeWatch(watch));
+    return watch;
 
-
-
+}
 
 // reducer  
 
 const initialState = {}
 
 const watchReducer = (state = initialState, action) => {
-
+    switch (action.type) {
+        case ADD_WATCH:
+            return {
+                ...state,
+                [action.watch.id]: action.watch
+            }
+        case LOAD_WATCH:
+            return {
+                ...state,
+                ...Object.fromEntries(action.watch.map((item) => [item.id, item]))
+            }
+        case REMOVE_WATCH:{
+            const newState = {...state};
+            delete newState[action.watch];
+            return newState;
+        }
+    default: 
+        return state;
+    }
+   
 } 
+
+export default watchReducer;
