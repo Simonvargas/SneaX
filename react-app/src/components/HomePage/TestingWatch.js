dd8ae18213503108f54347e8968f92f3b26bd310
simonvargas <simonvargas01@gmail.com>
2021-08-18 14:11:32 -0400

    push

--- react-app/src/components/HomePage/TestingWatch.js
+++ react-app/src/components/HomePage/TestingWatch.js
@@ -5,18 +5,20 @@ import { Link } from 'react-router-dom';
 import * as sessionActions from "../../store/session";
 import { getWatchs } from '../../store/watch';
 import { allSneax } from '../../store/sneax';
-
+import { getList } from '../../store/watchlist'
 
 // import { getList } from '../../store/watchlist';
 
 function TestingWatch () {
     const dispatch = useDispatch ();
 
+    const watchlists = Object.values(useSelector(state => state.watchlist))
     const sessionUser = useSelector(state => state.session.user)
     const allWatchs = Object.values(useSelector(state => state.watch))
     const sneaxs = useSelector((state) => Object.values(state.sneax))
     const sneaxId = sneaxs.map(sneax => sneax.id)
-    console.log("WHATTTT", sneaxs)
+    console.log("WHATTTT", watchlists)
+    // console.log("NOOOO", allWatchs)
 // if sneaxId === watch.sneax_id then return sneax.name?? trying to figure how to get the name of the sneax to populate 
    
 
@@ -24,8 +26,10 @@ function TestingWatch () {
     useEffect(() => {
         dispatch(allSneax());
         dispatch(getWatchs(sessionUser.id));
+        dispatch(getList())
     }, [dispatch, sessionUser.id]);
 
+    console.log(watchlists)
     return (
         <div className="testing-container">
             <h1>Hello testing page</h1>
