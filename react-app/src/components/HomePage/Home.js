bf1314df28b7aff1c0f644616dad3228c112dd40
Vivian C <baongocche@gmail.com>
2021-08-19 08:12:11 -0700

    dashboard styling

--- react-app/src/components/HomePage/Home.js
+++ react-app/src/components/HomePage/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory, useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import * as sessionAction from '../../store/session';
 import * as shareAction from '../../store/shares';
@@ -260,39 +260,81 @@ function Home() {
     main = (
       <>
       <NavBar/>
-      <div>
-      <h2>Shares</h2>
-        {shares?.map(share => {
-                if (Number(share.sneax_id)) {
-                  return (
-                  <ul>
-                    <li>
-                      <strong>Sneax name: {share.sneax.name}</strong>
-                    </li>
-                    <li>
-                      <strong>Price: $ {share.price_per_share}</strong>
-                    </li>
-                    <li>
-                      <strong>quantity of shares: {share.number_of_shares}</strong>
-                    </li>
-                    <li>
-                      <strong>total position: ${share.number_of_shares * share.price_per_share}</strong>
-                    </li>
-                    <button
-                      onClick={() => (reset(), setShowEdit(false), setShowTrade(!showTrade), setSellId(share.sneax_id), setSellQty(share.number_of_shares), setTotalPosition(share.number_of_shares * share.price_per_share), setShareQty(share.number_of_shares), setSharePrice(share.price_per_share), setShareId(share.id))}
-                    >trade</button>
-                    <div hidden="true">
-                      {totalAccount += (share.number_of_shares * share.price_per_share)}.
-                    </div>
-                  </ul>
-        )}})}
-        <TestingWatch />
+      <div className="sneax-dashboard-container">
+
+        <div className= 'sneax-shares-container'>
+          <div className='dash-shares-container'>
+            <div className='dash-shares-box'>
+              <div className='shares'>
+                <h1>Shares</h1>
+              </div>
+                {shares?.map(share => {
+                        if (Number(share.sneax_id)) {
+                          return (
+                              <div className='single-sneax-container'>
+                                <Link className="sneax-link" style={{ textDecoration: 'none' , color: 'black'}} to={`/sneax/${share.sneax_id}`}>
+                                  <div className='sneax-name-shares'>
+                                    <div className='share-sneax-name'><p>{share.sneax.name}</p></div>
+                                    <div className='sneax-shares-price'><p>$ {share.price_per_share}</p></div>
+                                  </div>
+                                  <div className='sneax-price-total'>
+                                  <div className='sneax-price'><p>{share.number_of_shares} Shares</p></div>
+                                  <div className='sneax-total'><p>Total Position: ${share.number_of_shares * share.price_per_share}</p></div>
+                                  </div>
+                                </Link>
+                                <div className='trade-bttn'>
+                                  <button onClick={() => (
+                                      reset(),
+                                      setShowEdit(false),
+                                      setShowTrade(!showTrade),
+                                      setSellId(share.sneax_id),
+                                      setSellQty(share.number_of_shares),
+                                      setTotalPosition(share.number_of_shares * share.price_per_share),
+                                      setShareQty(share.number_of_shares), setSharePrice(share.price_per_share),
+                                      setShareId(share.id))}>Trade
+                                  </button>
+                                </div>
+                                <div hidden="true">
+                                  {totalAccount += (share.number_of_shares * share.price_per_share)}.
+                                </div>
+                              </div>
+
+                          )
+                        }
+                })}
+              </div>
+              <TestingWatch />
+          </div>
+
+
+
+        </div>
+        <div clasName='sneax-dash-container'>
+          <div className='sneax-info-container'>
+            <div className='sneax-dash-info-box'>
+              <div className='sneax-name-brand-box'>
+                <div className='name'><h1 className="sneax-name">$ {totalAccount}</h1></div>
+                <h2 className='sneax-brand'>$200 2.00% Today</h2>
+              </div>
+              <div className='sneax-graph'>
+                <h1>PUT GRAPH HERE</h1>
+              </div>
+              <div className='buying-power-container'>
+                <div><h1 className='dash-buying-power'>Buy Power</h1></div>
+                <div><h1 className='dash-wallet'>$ {wallet}</h1></div>
+              </div>
+              <div className='random-dash'>
+                <p>For more information, see our Privacy Policy.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      {
+      {/* {
         wallet ? [<h2>Total buying power: {wallet}</h2>, <h2>Total investing: {totalAccount} </h2> ]: null
           //   <h2>total account value: </h2>
-      }
+      } */}
       {content}
     </>
     )
