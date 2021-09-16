1e5865c96bf7bea462720af1655f9a27e3a91f31
june <luo.jnny@gmail.com>
2021-09-16 14:58:02 -0700

    footer inprogress

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -13,16 +13,29 @@ const Footer = () => {
             </div>
             <div className='teams'>
                 <div className='Joon'>
+                    <h1 className='team-name'>Joon C.</h1>
+                    <a className='footerLink'  href='https://github.com/88joonyc' target='_blank' rel='noreferrer'>
+                        <i class='fab fa-github fa-2x' />
+                    </a>
 
                 </div>
                 <div className='Jun'>
-
+                    <h1 className='team-name'>Jun L.</h1>
+                    <a className='footerLink'  href='https://github.com/88joonyc' target='_blank' rel='noreferrer'>
+                        <i class='fab fa-github fa-2x' />
+                    </a>
                 </div>
                 <div className='Simon'>
-
+                    <h1 className='team-name'>Simon V.</h1>
+                    <a className='footerLink'  href='https://github.com/Simonvargas' target='_blank' rel='noreferrer'>
+                        <i class='fab fa-github fa-2x' />
+                    </a>
                 </div>
                 <div className='Vivian'>
-
+                    <h1 className='team-name'> Vivian C.</h1>
+                    <a className='footerLink'  href='https://github.com/itsvivrant' target='_blank' rel='noreferrer'>
+                        <i class='fab fa-github fa-2x' />
+                    </a>
                 </div>
 
 
