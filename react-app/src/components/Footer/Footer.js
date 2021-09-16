c4269cfc59dd2fb6c50a8d89e1e47674f01da6fb
june <luo.jnny@gmail.com>
2021-09-16 15:47:12 -0700

    footer completed

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -8,41 +8,63 @@ const Footer = () => {
         <div className='footerContainer'>
             <div className='Github'>
                 <a className='footerLink'  href='https://github.com/Simonvargas/SneaX' target='_blank' rel='noreferrer'>
-                    <i class='fab fa-github fa-2x' />
+                    <i className='fab fa-github fa-2x' />
                 </a>
             </div>
             <div className='team-members'>
-                <div className='Joon'>
-                    <h1 className='team-name'>Joon C.</h1>
+                
+                <div className='teammate jun'>
+                    <div>
+                    <h1 className='team-name'>Jun L.</h1>
+                    </div>
                     <div className='git-link-icons'>
                         <a className='footerLink'  href='https://github.com/88joonyc' target='_blank' rel='noreferrer'>
-                            <i class='fab fa-github fa-2x' />
+                            <i className='fab fa-github fa-2x' />
                         </a>
+                        <a className='footerLink' href='https://www.linkedin.com/in/itisjun/' target='_blank' rel='noreferrer'>
+                            <i className='fab fa-linkedin fa-2x'></i>
+                        </a> 
+                    </div>
+                </div>
+                <div className='teammate paul'>
+                    <div>
+                    <h1 className='team-name'>Paul C.</h1>
+                    </div>
+                    <div className='git-link-icons'>
+                        <a className='footerLink'  href='https://github.com/88joonyc' target='_blank' rel='noreferrer'>
+                            <i className='fab fa-github fa-2x' />
+                        </a>
+                        <a className='footerLink' href='https://www.linkedin.com/in/pchang1216/' target='_blank' rel='noreferrer'>
+                            <i className='fab fa-linkedin fa-2x'></i>
+                        </a>                       
                     </div>
 
                 </div>
-                <div className='Jun'>
-                    <h1 className='team-name'>Jun L.</h1>
-                    <div className='git-link-icons'>
-                        <a className='footerLink'  href='https://github.com/88joonyc' target='_blank' rel='noreferrer'>
-                            <i class='fab fa-github fa-2x' />
-                        </a>
-                    </div>
-                </div>
-                <div className='Simon'>
+                <div className='teammate simon'>
+                    <div>
                     <h1 className='team-name'>Simon V.</h1>
+                    </div>
                     <div className='git-link-icons'>
                         <a className='footerLink'  href='https://github.com/Simonvargas' target='_blank' rel='noreferrer'>
-                            <i class='fab fa-github fa-2x' />
+                            <i className='fab fa-github fa-2x' />
                         </a>
+                        <a className='footerLink' href='https://www.linkedin.com/in/simon-vargas-aa0b6a14b/' target='_blank' rel='noreferrer'>
+                            <i className='fab fa-linkedin fa-2x'></i>
+                        </a> 
+                    
                     </div>
                 </div>
-                <div className='Vivian'>
+                <div className='teammate vivian'>
+                    <div>
                     <h1 className='team-name'> Vivian C.</h1>
+                    </div>
                     <div className='git-link-icons'>
                         <a className='footerLink'  href='https://github.com/itsvivrant' target='_blank' rel='noreferrer'>
-                            <i class='fab fa-github fa-2x' />
+                            <i className='fab fa-github fa-2x' />
                         </a>
+                        <a className='footerLink' href='https://www.linkedin.com/in/vivian-ngoc-che/' target='_blank' rel='noreferrer'>
+                            <i className='fab fa-linkedin fa-2x'></i>
+                        </a> 
                     </div>
                 </div>
 
